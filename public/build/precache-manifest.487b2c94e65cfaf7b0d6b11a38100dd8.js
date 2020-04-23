e0b94cceabedc8777084085a0b7b94c6aacc1ae8
Siddhartha Saxena <siddharthasaxena.1998@gmail.com>
2020-04-23 17:12:20 +0530

    Loading Spinners

---
+++ public/build/precache-manifest.487b2c94e65cfaf7b0d6b11a38100dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfd27aa0a0e3e4e339dbe09164d8644",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d261fa14b508d49e9ae",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8992764d121565a25c46",
+    "url": "/static/js/2.3c8b5bef.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.3c8b5bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d261fa14b508d49e9ae",
+    "url": "/static/js/main.5ac1d5cf.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
